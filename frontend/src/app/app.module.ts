6babcab2cf5ea1af1229298b4584d63feb6918a1
bjackson14 <ben.jksn@gmail.com>
2019-07-20 22:29:35 -0400

    Set up front end routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { LatlngComponent } from './components/latlng/latlng.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'latlng', component: LatlngComponent },
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { LatlngComponent } from './components/latlng/latlng.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
