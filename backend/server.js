5337534fc1ef817d14a4a2904ee2043b34a4c49d
bjackson14 <ben.jksn@gmail.com>
2019-07-21 22:17:39 -0400

    Added check for username when adding a new user so duplicate usernames are not created.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connects to the MongoDB
-mongoose.connect('mongodb://localhost:27017/users');
+mongoose.connect('mongodb://localhost:27017/weather-api');
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Connected to MongoDB');
@@ -26,11 +26,14 @@ router.route('/').get((req, res) => {
 
 // Sets up route to get one login
 router.route('/login/:username').get((req, res) => {
+    // Checks if username exists
     Login.find({'username': req.params.username}, (err, login) => {
         if (err)
             console.log(err);
+        // If username doesn't exist send error
         else if (login.length === 0)
             res.status(400).send('No user found');
+        // If username exists send login information in json format
         else
             res.json(login);
     });
@@ -39,14 +42,25 @@ router.route('/login/:username').get((req, res) => {
 // Route added to create new user, but will not be available from Angular front end as of yet and might never be
 // Please use Postman or ARC to add new users
 router.route('/login/add').post((req, res) => {
-    let login = new Login(req.body);
-    login.save()
-        .then(login => {
-            res.status(200).json({'login': 'New user added'});
-        })
-        .catch(err => {
-            res.status(400).send('Failed add new user');
-        });
+    // Checks if username exists
+    Login.find({'username': req.body.username}, (err, username) => {
+        if (err)
+            console.log(err);
+        // If username doesn't exist, create new login
+        else if (username.length === 0) {
+            let login = new Login(req.body);
+            login.save()
+                .then(login => {
+                    res.status(200).json({'login': 'New user added'});
+                })
+                .catch(err => {
+                    res.status(400).send('Failed add new user');
+                });
+        }
+        // Sends error if username exits
+        else
+            res.status(400).send('Duplicate username');
+    });
 });
 
 // Adds routes to base url
