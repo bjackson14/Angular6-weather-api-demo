86f2847d9a8ed53cf3fd84ee0a75221c350c2ef3
bjackson14 <ben.jksn@gmail.com>
2019-07-23 01:06:52 -0400

    Added error handling for bad login and formated spacing better

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+
 import { LoginService } from '../../login.service';
 import { Login } from '../../login.model';
 
@@ -13,7 +15,7 @@ export class LoginComponent implements OnInit {
 
   login: Login; // Interface to hold login ingormation retrieved from server
   loginForm: FormGroup; // Group for login form elements
-  loginError: Boolean; // Used for test to determine if error message should be displayed
+  loginError: Boolean; // Used to check if there was an error in http request
 
   constructor(private loginService: LoginService, private router: Router, private formBuilder: FormBuilder) { 
     // Creates login form and adds validators
@@ -22,7 +24,7 @@ export class LoginComponent implements OnInit {
       password: ['', Validators.required]
     });
 
-    // Set to false at start for no login error
+    // Initiaizes loginError to show no error
     this.loginError = false;
   }
 
@@ -30,19 +32,21 @@ export class LoginComponent implements OnInit {
   }
 
   // Gets the login information
-  getLogin(username: String, password: String) {
+  loginToSite(username: String, password: String) {
     // Gets login information based on username
     this.loginService.getLoginByUsername(username).subscribe((login: Login) => {
       // Sets response to login interface
       this.login = login;
 
       // Checks if the passwordin the interface is the same as password provided by user
-      // If passwords do not match, sets error to true so html will display an error message
-      if (this.login.password !== password)
-        this.loginError = true;
       // If passwords match, routes user to latlng page
-      else
+      if (this.login.password === password)
         this.router.navigate(['/latlng']);
+      else
+        this.loginError = true;
+    },
+    (err: HttpErrorResponse) => {
+      this.loginError = true;
     });
   }
 
--- frontend/src/app/login.service.ts
+++ frontend/src/app/login.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Login } from './login.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  uri: String = 'http://localhost:3000';
+  uri: String = 'http://localhost:3000'; // Uri with express server
 
   constructor(private http: HttpClient) { }
 
   // Returns the login information retrieved by the username
-  getLoginByUsername(username: String) {
-    return this.http.get(`${this.uri}/login/${username}`);
+  getLoginByUsername(username: String): Observable<any> {
+    return this.http.get<Login>(`${this.uri}/login/${username}`)
+      .pipe(catchError(this.errorHandler));
   }
 
   // Adds new login information
-  addLogin(username: String, password: String) {
-    const login = {
+  addLogin(username: String, password: String): Observable<any> {
+    const login: Login = {
       username: username,
       password: password
     };
-    return this.http.post(`${this.uri}/login/add`, login);
+    return this.http.post<Login>(`${this.uri}/login/add`, login)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  // Throws error from http request
+  errorHandler(err: HttpErrorResponse): Observable<HttpErrorResponse> {
+    return throwError(err);
   }
 }
